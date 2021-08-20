3afbb0a0d677bb44a5ba48b3cd97488d35ee4c1a
David Mallon ⚙ <mallond@gmail.com>
2021-08-20 14:46:38 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "linode" {
     token = "5f1eb0972c8fd58d5f12b325db18e982b4b97a2e1330715c667898b61ce72c7c"
 }
 
-resource "linode_instance" "Ubuntu K8S" {
+resource "linode_instance" "ubuntu_k8s" {
     label = "ubuntu_k8s"
     image = "linode/ubuntu18.04"
     region = "us-central"
