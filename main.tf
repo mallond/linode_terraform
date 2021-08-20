2f57ed87cf552acaa61050fa420e5a01d21999ca
David Mallon ⚙ <mallond@gmail.com>
2021-08-20 15:54:25 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,6 @@ resource "linode_instance" "ubuntu_k8s" {
     image = "linode/ubuntu18.04"
     region = "us-central"
     type = "g6-standard-1"
-    authorized_keys = ["${authorized_keys}"]
+    authorized_keys = [""]
     root_pass = "RootPassword$4"
 }
