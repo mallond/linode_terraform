c66efc04d7fa2f84eb2ca04437d7a50439444b9c
David Mallon ⚙ <mallond@gmail.com>
2021-08-24 12:24:42 -0700

    Update install_microk8s.sh

--- install_microk8s.sh
+++ install_microk8s.sh
@@ -2,7 +2,7 @@
 apt-get update
 snap install microk8s --classic --channel=1.21/stable
 microk8s status --wait-ready
-microk8s enable dashboard dns registry helm ingress
+microk8s enable dashboard dns registry helm ingress prometheus grafana 
 snap alias microk8s.kubectl kubectl    
 usermod -a -G microk8s root
 chown -f -R root ~/.kube
