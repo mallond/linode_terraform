49a3c3b09fea4c959a1f2ec74a1fcec096987d0d
David Mallon ⚙ <mallond@gmail.com>
2021-08-24 10:21:28 -0700

    Update local-exec.sh

--- local-exec.sh
+++ local-exec.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# Avoid spaces around the assignment operator (=)
-var="Hello World"
+apt update
+snap install microk8s --classic --channel=1.21/stable
+microk8s status --wait-ready
+microk8s enable dashboard dns registry helm ingress
+snap alias microk8s.kubectl kubectl    
+usermod -a -G microk8s cloud_user
+chown -f -R cloud_user ~/.kube
  
-# print it 
-echo "$var"
