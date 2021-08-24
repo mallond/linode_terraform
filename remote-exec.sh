b2d483b00dfab80e944cd381ad2beebdafc17be5
David Mallon ⚙ <mallond@gmail.com>
2021-08-24 11:13:01 -0700

    Update remote-exec.sh

--- remote-exec.sh
+++ remote-exec.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 apt-get update
-//snap install microk8s --classic --channel=1.21/stable
-//microk8s status --wait-ready
-//microk8s enable dashboard dns registry helm ingress
-//snap alias microk8s.kubectl kubectl    
-//usermod -a -G microk8s cloud_user
-//chown -f -R cloud_user ~/.kube
+snap install microk8s --classic --channel=1.21/stable
+microk8s status --wait-ready
+microk8s enable dashboard dns registry helm ingress
+snap alias microk8s.kubectl kubectl    
+usermod -a -G microk8s cloud_user
+chown -f -R cloud_user ~/.kube
  
