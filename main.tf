61bb1452891755b1f9dc679342d1f0a6e20e1ffd
David Mallon ⚙ <mallond@gmail.com>
2021-08-24 10:09:18 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,15 +19,7 @@ resource "linode_instance" "ubuntu_k8s" {
     authorized_keys = ["ssh-rsa AAAA...Gw== user@example.local"]
     root_pass = "RootPassword$4"
   provisioner "remote-exec" {
-    inline = [
-      "apt update",
-      "snap install microk8s --classic --channel=1.21/stable",
-      "microk8s status --wait-ready",
-      "microk8s enable dashboard dns registry helm ingress",
-      "snap alias microk8s.kubectl kubectl",    
-      "usermod -a -G microk8s root",
-      "chown -f -R root ~/.kube"
-    ]
+    command = "./local-exec.sh"
   }
 }
 
