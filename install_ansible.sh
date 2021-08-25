2f96f7322d8629e9895923f7f16655d559445c70
David Mallon ⚙ <mallond@gmail.com>
2021-08-25 13:12:32 -0700

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-snap install ansible-kz6fittycent --edge --jailmode
+apt install software-properties-common
+add-apt-repository --yes --update ppa:ansible/ansible
+apt install ansible -y
