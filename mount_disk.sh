845ca465c65a861c478c8e5247072fd6ec55c922
David Mallon ⚙ <mallond@gmail.com>
2021-08-25 10:37:23 -0700

    Update mount_disk.sh

--- mount_disk.sh
+++ mount_disk.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # mkfs.ext4 "/dev/disk/by-id/scsi-0Linode_Volume_mydata"
-mkdir "/mnt/mydata"
-mount "/dev/disk/by-id/scsi-0Linode_Volume_mydata" "/mnt/mydata"
-/dev/disk/by-id/scsi-0Linode_Volume_mydata /mnt/mydata ext4 defaults,noatime,nofail 0 2
+# mkdir "/mnt/mydata"
+# mount "/dev/disk/by-id/scsi-0Linode_Volume_mydata" "/mnt/mydata"
+# /dev/disk/by-id/scsi-0Linode_Volume_mydata /mnt/mydata ext4 defaults,noatime,nofail 0 2
+echo "You will have to mount this manually -- for now, sorry!"
