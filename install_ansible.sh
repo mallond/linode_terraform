8476814797a8401590c452845e7caea48038d258
David Mallon ⚙ <mallond@gmail.com>
2021-08-25 12:45:34 -0700

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get install software-properties-common
-apt-get add-apt-repository --yes --update ppa:ansible/ansible
+# apt-get add-apt-repository --yes --update ppa:ansible/ansible
 apt-get install ansible -y
