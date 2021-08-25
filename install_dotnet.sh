66f24322b2d0487051396b751fe2fac7fa7b1b68
David Mallon ⚙ <mallond@gmail.com>
2021-08-25 15:56:09 -0700

    Update install_dotnet.sh

--- install_dotnet.sh
+++ install_dotnet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 snap install dotnet-sdk --classic --channel=5.0
 snap alias dotnet-sdk.dotnet dotnet
-export DOTNET_ROOT=/snap/dotnet-sdk/current
+sed -i "$ a export PATH=$PATH:/snap/bin" .profile
